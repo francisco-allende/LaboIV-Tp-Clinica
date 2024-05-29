a28b554e5bbe50be7908c9dc5b092d18d7fd0e4b
fallende <franciscoallende@lakaut.com.ar>
2024-05-28 22:06:04 -0300

    base proyecto

---
+++ clinica/src/app/app.config.ts
@@ -0,0 +1,11 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"clinica-3c199","appId":"1:843286763763:web:20326de4d5d4168fcebf86","storageBucket":"clinica-3c199.appspot.com","apiKey":"AIzaSyARVYnrMWCFZLB64LouAFTOxtRpl__n98s","authDomain":"clinica-3c199.firebaseapp.com","messagingSenderId":"843286763763"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+};
